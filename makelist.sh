ceec58bb6b00ddbc47dfb5584652b88420c0210f
Steve Stepp <steppsr@gmail.com>
2022-10-13 19:24:23 -0500

    Only output Wallet Address to file, NFT ID & Address to Screen

--- makelist.sh
+++ makelist.sh
@@ -10,16 +10,14 @@ nft_ids=`cat list.txt`
 n=1
 for id in $nft_ids; do
 
-  echo "$n. $id"
 
   nft_json=`curl -s https://api.mintgarden.io/nfts/$id`
   nft_id=`echo "$nft_json" | jq '.encoded_id' | cut -c 2- | rev | cut -c 2- | rev`
   nft_owner_wallet=`echo "$nft_json" | jq '.owner_address.encoded_id' | cut -c 2- | rev | cut -c 2- | rev`
-  echo "$nft_owner_wallet"
-  echo ""
-
-  echo "$nft_id $nft_owner_wallet" >> $appdir/results.txt
+  echo "$n. $id $nft_owner_wallet"
+  echo "$nft_owner_wallet" >> $appdir/results.txt
 
   n=$(($n+1))
 
 done
+
